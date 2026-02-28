24488d2de5c21981d78bd168f3369abcb9a2a209
Gengyao Qiu <aylwingun@outlook.com>
2026-02-28 03:54:27 -0500

    Model update

--- energnn.py
+++ energnn.py
@@ -47,12 +47,12 @@ class Toolbelt:
     @staticmethod
     def ase_atoms_to_graph(atoms:ase.atoms.Atoms, cutoff:float=30.0) -> tuple[tc.Tensor, tc.Tensor, tc.Tensor]:
         'This function will turn the ase Atoms into Data'
-        # return Data(node_feature=..., edge_index=..., edge_weight=...) # this is the input of model.
+        # return Data(node_feature=..., edge_index=..., edge_weight=..., cell_vectors=) # this is the input of model.
         atomic_numbers = tc.tensor(atoms.get_atomic_numbers(), dtype=tc.float)
-        atom_position = tc.tensor(atoms.get_positions(), dtype=tc.float)
+        atom_fractional_position = tc.tensor(atoms.get_scaled_positions(), dtype=tc.float)
         node_features = tc.cat([
             atomic_numbers.reshape(-1, 1),
-            atom_position], # TODO: atom_position should be fractional coordinates or cartesian coordinates? I think fractional coordinates is better, but I need to test it.
+            atom_fractional_position],
             dim=1)
         # i is the index of central atom, and j is the index of nearby atoms, d is the distance between each other.
         i, j, d = neighbor_list('ijd', atoms, cutoff, self_interaction=True)
@@ -64,7 +64,8 @@ class Toolbelt:
         return (
             node_features,
             edge_index,
-            edge_weight
+            edge_weight,
+            atoms.get_cell().tolist(),
         )
     @staticmethod
     def get_root_path()->str:
@@ -195,7 +196,7 @@ class DatasetAlexandria(Dataset):
             pickle.dump(self, f)
         return
 
-class DatasetWBM(Dataset):
+class DatasetWBM(Dataset): 
     def __init__(self, cutoff=5.0):
         print("Init the Dataset WBM.")
         from ase.io import read as ase_read # This function read .extxyz file to ase.atoms.Atoms
@@ -225,23 +226,28 @@ class DatasetWBM(Dataset):
         if type(index)==slice: raise Exception("Not support slice currently")
         node_features, edge_index, edge_weight = Toolbelt.ase_atoms_to_graph(self.wbm_relax_dataset_list[index][0], cutoff=self.cutoff)
         material_id:str = self.wbm_relax_dataset_list[index][1]
-        #df_wbm_summary_indexed = self.df_wbm_summary.set_index('material_id')
-        #e_above_hull_wbm = self.df_wbm_summary_indexed.loc[material_id]['e_above_hull_wbm']
-        #e_form_per_atom_wbm = self.df_wbm_summary_indexed.loc[material_id]['e_form_per_atom_wbm']
         e_above_hull_wbm = self.e_above_hull_wbm[material_id]
         e_form_per_atom_wbm = self.e_form_per_atom_wbm[material_id]
         uncorrected_energy = self.uncorrected_enery[material_id]
         return Data(
-            node_features = node_features, 
+            x = node_features, 
             edge_index = edge_index,
-            edge_weight = edge_weight,
+            edge_attr = edge_weight,
+            matrix = tc.tensor(self.wbm_relax_dataset_list[index][0].get_cell().tolist()).float(),
+            # TODO: Add stress and force to WBM dataset.
             y = tc.tensor([[uncorrected_energy, e_above_hull_wbm]]).float()
         )
 
-def datasetAlexandriaNeo(load_files:list[int]=["000"], cutoff=6.0)->list[Data]:
+def datasetAlexandriaNeo(load_files:list[str]=["000"], cutoff=6.0)->list[Data]:
     """
     This function will return a list as dataset.
-    - return: list[Data(x, edge_index, edge_attr, matrix, abc, y)]
+    - **load_files**: A list of file number from "000"-"049", the number in list will be load in one dataset.
+    - **cutoff**: The maximum length of bond.
+    - **return**: list[Data(x, edge_index, edge_attr, matrix, abc, y)]
+        - x: node features, shape [n, 4], where 4 for (atomic_number, fractional_x, fractional_y, fractional_z)
+        - edge_index: shape [2, m], where m is the number of edges.
+        - edge_attr: I think this may not been used in future because we already have node_features and matrix. But I still keep it.
+        - matrix: the lattice matrix of the structure, shape [3, 3]
     """
     print("Loading Alexandria dataset ==== ")
     def structure_to_graph_data(structure, cutoff=5.0):
@@ -262,138 +268,34 @@ def datasetAlexandriaNeo(load_files:list[int]=["000"], cutoff=6.0)->list[Data]:
         
         # 2. Generate Node Features.
         # You can add your node features here
-        node_features = torch.tensor([np.insert(site.coords, 0, site.specie.Z) for site in structure], dtype=torch.float).view(-1, 4) # [<atomic_number>, <x>, <y>, <z>]
+        node_features = torch.tensor([np.insert(site.frac_coords, 0, site.specie.Z) for site in structure], dtype=torch.float).view(-1, 4) # [<atomic_number>, <fractional x>, <fractional y>, <fractional z>]
         # Generate the matrix
         matrix = torch.tensor(structure.lattice.matrix)
-        abc = torch.tensor([atom.frac_coords for atom in structure]).view(-1, 3)
-        return node_features, edge_index, edge_weight, matrix, abc
+        xyz = torch.tensor([atom.coords for atom in structure]).view(-1, 3)
+        return node_features, edge_index, edge_weight, matrix, xyz
     from monty.serialization import loadfn
     ans = []
     for i in load_files:
         file_path = f"{Toolbelt.get_root_path()}/tmp/Alexandria/alexandria_{i}.json"
         data_dict = loadfn(file_path)
         for entry in tqdm.tqdm(data_dict['entries']): # in each entry
-            node_features, edge_index, edge_weight, matrix, abc = structure_to_graph_data(entry.structure, cutoff=cutoff)
+            node_features, edge_index, edge_weight, matrix, xyz = structure_to_graph_data(entry.structure, cutoff=cutoff)
+            stress = entry.data['stress']
             ans.append(Data(
-                x = node_features, # [<atomic_number>, <x>, <y>, <z>] # TODO: the position should be fractional coordinates or cartesian coordinates? I think fractional coordinates is better, but I need to test it.
+                x = node_features, # [<atomic_number>, <fractional_x>, <fractional_y>, <fractional_z>]
                 edge_index = edge_index,
                 edge_attr = edge_weight,
                 matrix = matrix,
-                abc = abc,
+                stress = stress, # The stress of the structure.
+                xyz = xyz,
+                # TODO: Add force.
                 y = entry.energy, # The energy without correction.
             ))
     return ans
 
 
 # region: Modules
-class ModelBasicGNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = tgnn.GCNConv(4, 12)
-        self.conv2 = tgnn.GCNConv(12, 24)
-        self.conv3 = tgnn.GCNConv(24, 48)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(48, 32)
-        self.fc2 = nn.Linear(32, 16)
-        self.fc3 = nn.Linear(16, 2)
-        self.lk = nn.LeakyReLU(0.1)
-        pass
-
-    def forward(self, node_features:tc.Tensor, edge_index:tc.Tensor, edge_weight:tc.Tensor, batch_index:tc.Tensor) -> tc.Tensor:
-        """
-        Parameters:
-        - node_featurs: torch.Size([n, 4]) where 4 for (atomic_number, x, y, z)
-        - edge_distance:torch.Size([2, n])
-        - edge_weight:  torch.Size([n])
-        """
-        #breakpoint()
-        device = node_features.device
-        node_features = self.conv1(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        node_features = self.conv2(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        node_features = self.conv3(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        x = [tc.zeros((node_features.shape[1],), device=device) for _ in range(batch_index.max().item() + 1)]
-        for i in range(node_features.shape[0]):
-            x[batch_index[i]] = x[batch_index[i]] + node_features[i]
-        x = tc.stack(x, dim=0)
-        x = self.fc1(x)
-        x = self.lk(x)
-        x = self.fc2(x)
-        x = self.lk(x)
-        x = self.fc3(x)
-        return x
-    pass
-
-class ModelBasicGNNDeeper(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = tgnn.GCNConv(4, 12)
-        self.conv2 = tgnn.GCNConv(12, 86)
-        self.conv3 = tgnn.GCNConv(86, 492)
-        self.conv4 = tgnn.GCNConv(492, 492)
-        self.conv5 = tgnn.GCNConv(492, 492)
-        self.conv6 = tgnn.GCNConv(492, 1896)
-        self.conv7 = tgnn.GCNConv(1896, 1896)
-        self.conv8 = tgnn.GCNConv(1896, 1896)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(1896, 1292)
-        self.fc2 = nn.Linear(1292, 1292)
-        self.fc3 = nn.Linear(1292, 148)
-        self.fc4 = nn.Linear(148, 148)
-        self.fc5 = nn.Linear(148, 32)
-        self.fc6 = nn.Linear(32, 32)
-        self.fc7 = nn.Linear(32, 2)
-        self.lk = nn.LeakyReLU(0.1)
-        pass
-
-    def forward(self, node_features:tc.Tensor, edge_index:tc.Tensor, edge_weight:tc.Tensor, batch_index:tc.Tensor) -> tc.Tensor:
-        """
-        Parameters:
-        - node_featurs: torch.Size([n, 4]) where 4 for (atomic_number, x, y, z)
-        - edge_distance:torch.Size([2, n])
-        - edge_weight:  torch.Size([n])
-        """
-        #breakpoint()
-        device = node_features.device
-        node_features = self.conv1(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        node_features = self.conv2(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        node_features = self.conv3(node_features, edge_index, edge_weight)
-        node_features = tmp = self.lk(node_features)
-        node_features = self.conv4(node_features, edge_index, edge_weight)
-        node_features = tmp = self.lk(node_features) + tmp
-        node_features = self.conv5(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features) + tmp
-        node_features = self.conv6(node_features, edge_index, edge_weight)
-        node_features = tmp = self.lk(node_features)
-        node_features = self.conv7(node_features, edge_index, edge_weight)
-        node_features = tmp = self.lk(node_features) + tmp
-        node_features = self.conv8(node_features, edge_index, edge_weight)
-        node_features = self.lk(node_features)
-        x = [tc.zeros((node_features.shape[1],), device=device) for _ in range(batch_index.max().item() + 1)]
-        for i in range(node_features.shape[0]):
-            x[batch_index[i]] = x[batch_index[i]] + node_features[i]
-        x = tc.stack(x, dim=0)
-        x = self.fc1(x)
-        x = self.lk(x)
-        x = self.fc2(x)
-        x = self.lk(x)
-        x = self.fc3(x)
-        x = tmp = self.lk(x)
-        x = self.fc4(x)
-        x = self.lk(x) + tmp
-        x = self.fc5(x)
-        x = tmp = self.lk(x)
-        x = self.fc6(x)
-        x = self.lk(x) + tmp
-        x = self.fc7(x)
-        x = self.lk(x)
-        return x
-    pass
-
+# This model is depreciated.
 class EnerG(nn.Module):
     """This model only predic the total energy of a structure (graph) in forward."""
     def __init__(self):
@@ -415,10 +317,18 @@ class EnerG(nn.Module):
     def forward(self, data:Data):
         # we don't using edge_weight here, we calculate edge_weight from node_features.
         model_device = next(self.parameters()).device
-        edge_weight_cal = self.node_features_to_edge_weight(data.x, data.edge_index).to(model_device)
-        node_features = data.x.to(model_device)
-        edge_index = data.edge_index.to(model_device)
         batch_index = data.batch.to(model_device)
+        x = data.x.to(model_device)
+        matrix = data.matrix.to(model_device)
+        # Convert the fractional coordination to absolute.
+        def cvt_frac_to_abs(x, matrix, batch_index):
+            matrix_group = matrix.float().reshape(-1, 3, 3)
+            abs_position = (x[:, 1:].reshape(-1, 1, 3)@matrix_group[batch_index]).reshape(-1, 3)
+            ans = tc.cat((x[:, 0].reshape(-1, 1), abs_position), dim=1)
+            return  ans
+        node_features = cvt_frac_to_abs(x, matrix, batch_index)
+        edge_weight_cal = self.node_features_to_edge_weight(node_features, data.edge_index).to(model_device)
+        edge_index = data.edge_index.to(model_device)
         node_features = self.conv1(node_features, edge_index, edge_weight_cal)
         node_features = self.leaky(node_features)
         node_features = self.conv2(node_features, edge_index, edge_weight_cal)
@@ -437,13 +347,20 @@ class EnerG(nn.Module):
         return x
     def force(self, input_data:Data):
         """This function will evaluate the force of struct"""
+        # TODO: Convert the fractional coordination to absolute coordination.
         model_device = next(self.parameters()).device
-        node_features = input_data.x
+        edge_index = edge_index.to(model_device)
+        x = input_data.x
         edge_index = input_data.edge_index
         edge_weight = input_data.edge_attr
         batch_index = input_data.batch
-        node_features = node_features.detach().clone().to(model_device).requires_grad_(True)
-        edge_index = edge_index.to(model_device)
+        # Convert fractional coordination to absolute.
+        def cvt_frac_to_abs(x, matrix, batch_index):
+            matrix_group = matrix.float().reshape(-1, 3, 3)
+            abs_position = (x[:, 1:].reshape(-1, 1, 3)@matrix_group[batch_index]).reshape(-1, 3)
+            ans = tc.cat((x[:, 0].reshape(-1, 1), abs_position), dim=1)
+            return  ans
+        node_features = cvt_frac_to_abs(x, input_data.matrix, batch_index).detach().clone().to(model_device).requires_grad_(True)
         edge_weight = edge_weight.detach().clone().to(model_device).requires_grad_(True)
         batch_index = batch_index.to(model_device)
         node_features.grad.zero_() if node_features.grad!=None else "Do nothing"# clean the grad
@@ -494,18 +411,13 @@ def tcg_tester(model:tc.nn.Module, dataset:Dataset, loss_fn,# loss_fn here shoul
                batch_size=64,
                num_workers=1):
     """This tester apply the tcg_loader.dataloader to dataset"""
-    # TODO: model forward should be a Data type.
     loss_fn = loss_fn()
     with tc.no_grad(): # Test don't need the grad
         model = model.to(device)
         dataloader = tcg_loader.DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
         for i in (bar:=tqdm.tqdm(dataloader)):
-            node_features = i.node_features.to(device)
-            edge_index = i.edge_index.to(device)
-            edge_weight = i.edge_weight.to(device)
             y = i.y.to(device)
-            batch_index = i.batch.to(device)
-            pred = model(node_features, edge_index, edge_weight, batch_index)
+            pred = model(i)
             loss = loss_fn(y, pred)
             bar.set_description_str(f"loss_fn_output={loss}")            
     return
